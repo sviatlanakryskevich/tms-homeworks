8a33d3edfd2270add10e0af22e23ed3f909849ed
Sviatlana.Kryskevich <sviatlana.kryskevich@gmail.com>
2023-09-05 22:28:23 +0200

    hometask-lesson20

---
+++ lesson20/src/main/java/com/skv/servlets/Servlet1.java
@@ -0,0 +1,20 @@
+package com.skv.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/servlet1")
+public class Servlet1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello from Servlet1");
+        resp.addCookie(new Cookie("key", "value"));
+    }
+}
