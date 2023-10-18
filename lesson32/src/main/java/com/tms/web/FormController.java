8d53e3832a578a527b4e6d50512606e83dc64ea6
Sviatlana.Kryskevich <sviatlana.kryskevich@gmail.com>
2023-10-18 17:01:27 +0200

    hometask-lesson33-spring-web-modelAttribute

--- lesson32/src/main/java/com/tms/web/FormController.java
+++ lesson32/src/main/java/com/tms/web/FormController.java
@@ -18,25 +18,18 @@ public class FormController {
     private final UserService userService;
 
     @GetMapping
-    public ModelAndView formPage(){
+    public ModelAndView formPage(@ModelAttribute(name = "user") User user){
         ModelAndView modelAndView = new ModelAndView("form");
-      /*  modelAndView.addObject("users", userService.getUsers());*/
         return modelAndView;
     }
 
     @PostMapping
-    public ModelAndView addUser(@Valid User user, BindingResult result){
+    public ModelAndView addUser(@ModelAttribute(name = "user")@Valid User user, BindingResult result){
         ModelAndView modelAndView = new ModelAndView("form");
-        if(result.hasFieldErrors()){
-            result.getFieldErrors()
-                    .forEach(fieldError -> {
-                        String field = fieldError.getField();
-                        String defaultMessage = fieldError.getDefaultMessage();
-                        modelAndView.addObject(field + "Error", defaultMessage);
-                    });
-        }
+
         if(!result.hasFieldErrors()){
             userService.saveUser(user);
+            modelAndView.addObject("user", new User());
         }
 
         modelAndView.addObject("users", userService.getUsers());
